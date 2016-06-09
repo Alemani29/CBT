4ee3bb84bc4d4d6f63b3c110475786a671d487e8
Pau Alemany <alemani.pau@gmail.com>
2016-06-09 22:41:18 +0200

    Update CBT.py

--- CBT.py
+++ CBT.py
@@ -32,6 +32,38 @@ time.sleep(1)
 
 while True:
     inp = input(">>")
+
+
+        if inp == "bosc":
+            zona = 1
+            print("Ets al bosc. Aquí pots collir fruita")
+
+        if inp == "ciutat":
+            zona = 2
+            print("Ets a la ciutat. Aquí pots accedir a la botiga")          
+
+
+    if inp == "restart":
+        pinya = 0
+        pinyapelada = 0
+        poma = 0
+        money = 0
+        ganivet = 0
+        ganivetma = 0
+        zona = 1
+    
+        print("---CBT---")
+        print("Benvingut a CBT, un joc de text.")
+        print("Hauràs d'escriure de manera simple: acció+objecte")
+        print("Per el moment et dono les quatre frases bàsiques del joc:")
+        print("agafar poma")
+        print("agafar pinya")
+        print("menjar poma")
+        print("menjar pinya")
+        print("També pots veure el que portes a sobre amb: motxilla")
+        print("En aquests moments ets al bosc")
+        print("Bona sort!")
+    
     
     if inp == "motxilla":
             print("tens", poma, "poma/es")
@@ -39,22 +71,16 @@ while True:
             print("tens", pinyapelada, "pinya/es pelada/es")
             print("tens", money, "$")
 
-    if inp == "bosc":
-        zona = 1
-
-    if inp == "ciutat":
-        zona = 2
 
     if zona == 0:
         print("La pinya estava en mal estat")
-        time.sleep(1)
+        time.sleep(2)
         print("Has mort")
         print("-Game Over-")
         print("Escriu restart per tornar a començar")
 
-    if zona == 1:
-        print("Ets al bosc")
 
+    if zona == 1:
         if inp == "agafar poma":
             poma = poma+1
             print("tens", poma, "poma/es")
@@ -117,8 +143,6 @@ while True:
 
     
     if zona == 2:
-        print("Ets a la ciutat")
-
         if inp == "agafar poma":
             print("D'on agafes les pomes? Ets a la ciutat!")
             print("Has d'anar al bosc amb >>bosc")
@@ -128,57 +152,76 @@ while True:
             print("Has d'anar al bosc amb >>bosc")
                     
         if inp == "botiga":
+            zona = 4
             print("Benvingut a la botiga")
-            print("pots comprar amb >>comprar i el que vols comprar")
+            print("pots comprar amb >>comprar+objecte")
             print("poma - 1$")
             print("ganivet - 10$")
+            print("pinya - ?$")               
 
-            if inp == "comprar poma":
-                print("Costa 1$")
-                if money<=0:
-                    print("Com vols pagar?!")
-                    print("No tens suficients $!")
-                    print("proba a vendre")
-                else:
-                    poma = poma+1
-                    money = money-1
-                    print("Aquí tens")
-                    print("tens", poma, "poma/es")
-                    print("tens", money, "$")
-            
-            if inp == "comprar ganivet":
-                print("Costa 10$")
-                if money<=9:
-                    print("Com vols pagar?!")
-                    print("No tens suficients $!")
-                    print("proba a vendre")
-                else:
-                    ganivet = ganivet+1
-                    money = money-10
-                    print("Aquí tens")
-                    print("tens", ganivet, "ganivet/s")
-                    print("tens", money, "$")
-
-            if inp == "vendre poma":
-                if poma<=0:
-                    print("No pots tenir pomes negatives!")
-                else:
-                    poma = poma-1
-                    money = money+1
-                    print("Has venut una poma, guanyes 1$")
-                    print ("tens", poma, "poma/es")
-
-            if inp == "vendre pinya":
-                if pinya<=0:
-                    print("No pots tenir pinyes negatives!")
-                else:
-                    print("La venda de pinyes és il·legal")
-                    zona = 3   
         
     if zona == 3:
-        print("Has sigut arrestat, ara ets a la presó")
         ganivet = 0
         poma = 0
         pinya = 0
         money = 0
         pinyapelada = 0
+
+
+
+    if zona == 4:
+        if inp == "comprar poma":
+            print("Costa 1$")
+            if money<=0:
+                print("Com vols pagar?!")
+                print("No tens suficients $!")
+                print("proba a vendre amb >>vendre+objecte")
+            else:
+                poma = poma+1
+                money = money-1
+                print("Aquí tens")
+                print("tens", poma, "poma/es")
+                print("tens", money, "$")
+            
+        if inp == "comprar ganivet":
+            print("Costa 10$")
+            if money<=9:
+                print("Com vols pagar?!")
+                print("No tens suficients $!")
+                print("proba a vendre")
+            else:
+                ganivet = ganivet+1
+                money = money-10
+                print("Aquí tens")
+                print("tens", ganivet, "ganivet/s")
+                print("tens", money, "$")
+
+        if inp == "comprar pinya":
+                print("La compra de pinyes és il·legal")
+                time.sleep(2)
+                zona = 3
+                print("Has sigut arrestat, ara ets a la presó")
+                print("Tots els teus objectes han sigut requisats")
+
+        if inp == "vendre poma":
+            if poma<=0:
+                print("No pots tenir pomes negatives!")
+                print("Pots agafar pomes al bosc")
+                print("per anar al bosc utilitza >>bosc")
+            else:
+                poma = poma-1
+                money = money+1
+                print("Has venut una poma, guanyes 1$")
+                print ("tens", poma, "poma/es")
+
+        if inp == "vendre pinya":
+            if pinya<=0:
+                print("No pots tenir pinyes negatives!")
+                print("Pots agafar pinyes al bosc")
+                print("per anar al bosc utilitza >>bosc")
+            else:
+                print("La venda de pinyes és il·legal")
+                time.sleep(2)
+                zona = 3
+                print("Has sigut arrestat, ara ets a la presó")
+                print("Tots els teus objectes han sigut requisats")
