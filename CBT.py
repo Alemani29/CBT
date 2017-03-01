af418e114da9c52cd455366bb3249b95f65473d0
Pau Alemany <alemani.pau@gmail.com>
2017-03-01 23:34:52 +0100

    Add files via upload

--- CBT.py
+++ CBT.py
@@ -1,9 +1,15 @@
-#import files
+#Import files
 import intro
 import time
 import random
+import pygame
+from pygame.locals import *
 
-#variables
+#Pinya Colada Song
+pygame.mixer.init()
+
+
+#Variables
 pinya = 0
 pinyapelada = 0
 poma = 0
@@ -11,6 +17,19 @@ money = 0
 ganivet = 0
 ganivetma = 0
 zona = 1
+CBT = False
+Barra = False
+
+#Zones
+# ?.CBT
+# 0.End
+# 1.Bosc
+# 2.Ciutat
+# 3.Botiga
+# 4.Cel·la
+# 5.Presó
+# 6.Taverna
+
 
 intro.intro()
 while True:
@@ -30,7 +49,7 @@ while True:
 
     if inp == "ciutat":
         zona = 2
-        print("Ets a la ciutat. Aquí pots accedir a la botiga")
+        print("Ets a la ciutat. Escriu >>mapa per veure els llocs que pots visitar")
 
     if inp == "menjar poma":
         if poma<=0:
@@ -44,6 +63,32 @@ while True:
             print("No pots tenir pinyes negatives!")
         else:
             print("Tros de ruc! No has pelat la pinya!")
+
+    if inp == "pelar pinya":
+            if pinya<=0:
+                print("Com pots pelar una cosa que no tens?!")
+            else:
+                if ganivetma>=1:
+                    pinyapelada = pinyapelada+1
+                    pinya = pinya-1
+                    print("Has pelat la pinya")
+                    print ("tens", pinyapelada, "pinya/es pelada/es")
+                else:
+                    print("Mare meva! Però en què estàs pensant!")
+                    time.sleep(1)
+                    print("No has agafat ganivet!")
+
+    if inp == "agafar ganivet":
+        if ganivet>=1:
+            ganivetma = ganivetma+1
+            print("Ara si!")
+            time.sleep(1)
+            print("Ja pots pelar la pinya")
+        else:            
+            print("No tens ganivet")
+            time.sleep(1)
+            print("pots comprar un anant a la botiga")
+            print("proba amb >>botiga per veure el preu")
 
     if inp == "menjar pinya pelada":
         if pinyapelada<=0:
@@ -68,12 +113,14 @@ while True:
         print("La pinya estava en mal estat")
         time.sleep(2)
         print("Has mort")
+        time.sleep (2)
         print("-Game Over-")
         print("Escriu restart per tornar a començar")
 
 
     #Bosc
     if zona == 1:
+        
         if inp == "agafar poma":
             poma = poma+1
             print("tens", poma, "poma/es")
@@ -83,33 +130,7 @@ while True:
             print("tens", pinya, "pinya/es")
 
         if inp == "agafar pinya pelada":
-            print ("Que em prens per imbècil?!")            
-          
-        if inp == "pelar pinya":
-            if pinya<=0:
-                print("Com pots pelar una cosa que no tens?!")
-            else:
-                if ganivetma>=1:
-                    pinyapelada = pinyapelada+1
-                    pinya = pinya-1
-                    print("Has pelat la pinya")
-                    print ("tens", pinyapelada, "pinya/es pelada/es")
-                else:
-                    print("Mare meva! Però en què estàs pensant!")
-                    time.sleep(1)
-                    print("No has agafat ganivet!")
-
-        if inp == "agafar ganivet":
-            if ganivet>=1:
-                ganivetma = ganivetma+1
-                print("Ara si!")
-                time.sleep(1)
-                print("Ja pots pelar la pinya")
-            else:            
-                print("No tens ganivet")
-                time.sleep(1)
-                print("pots comprar un anant a la botiga")
-                print("proba amb >>botiga per veure el preu")
+            print ("Que em prens per imbècil?!")
 
         if inp == "botiga":
             print("Estàs en un bosc, per comprar, vés a la ciutat")
@@ -125,6 +146,13 @@ while True:
         if inp == "agafar pinya":
             print("D'on agafes les pinyes? Ets a la ciutat!")
             print("Has d'anar al bosc amb >>bosc")
+
+        if inp == "mapa":
+            print("pots anar a:")
+            print(">>botiga")
+            print(">>taverna")
+            print(">>port")
+            print(">>palau")
                     
         if inp == "botiga":
             zona = 3
@@ -132,7 +160,23 @@ while True:
             print("pots comprar amb >>comprar+objecte")
             print("poma - 1$")
             print("ganivet - 50$")
-            print("pinya - ?$")               
+            print("pinya - ?$")
+
+        if inp == "taverna":
+            print("Benvingut a la teberna de l'OGRE CRIDANER")
+            time.sleep(1)
+            print("Aqui pots trobar tot tipus de viatgers:")
+            print("mercaders, trobadors, soldats i exploradors, entre altres")
+            time.sleep(2)
+            print("Si vas a la barra (>>barra) pots demanar tant menjar com begudes")
+            print("Ara ets a la teva taula")
+            zona = 6
+
+        if inp == "port":
+            print("Aquesta zona està en obres torna en un futur per poder-la visitar")
+
+        if inp == "palau":
+            print("Aquesta zona està en obres torna en un futur per poder-la visitar")
 
     
     #Botiga
@@ -244,12 +288,60 @@ while True:
             zona = 2
 
 
-    #CBT
-    if random.randint(0,1000) <1:
-        time.sleep(2)
-        print("Ha aparegut una Cabra Boja Tridimensional!!!")
-        time.sleep(1)
-        print("Això és un event únic!")
-        time.sleep(1)
-        print("Ràpid atrapa-la abans de que escapi!")            
+    #Taverna
+    if zona == 6:
+
+        if inp == "barra":
+            print("Ets a la barra. Pots prendre:")
+            time.sleep(1)
+            print(">>Aigua")
+            print(">>Cervesa")
+            print(">>Pinya Colada")
+            time.sleep(2)
+            print("Que vols demanar?")
+            Barra = True
+
+        if Barra == True:
+                        
+            if inp == "pinya colada":
+                pygame.mixer.music.load("songs\pinyacolada.mp3")
+                pygame.mixer.music.play()
+                while pygame.mixer.music.get_busy():
+                    time.clock()
+                    
+                print("Has pres una Pinya Colada! No cal que paguis, convida la casa")
+                print("Has tornat a la teva taula")
+                Barra = False
+                
+
+
+     #CBT
+    if CBT == False:
+        
+        if random.randint(0,1000) <1:
+            time.sleep(2)
+            print("Ha aparegut un CBT! Una Cabra Boja Tridimensional!!!")
+            time.sleep(1)
+            print("Això és un event únic!")
+            time.sleep(1)
+            print("Ràpid atrapa-la abans que escapi! Fes servir >>atrapar CBT")
+            CBT = True
+
+    #Atrapa CBT
+    if CBT == True:
+
+        if inp == "atrapar CBT":
+            print("Però a que jugues?! És un CBT serà molt més difícil que escriure atrapar CBT!")
+            time.sleep(1)
+            print("proba a agafar una pakeball! Si, el CBT és un Pakemun interdimensional")
+
+        if inp == "agafar pakeball":
+            print("Evidentment tens una pakeball! Has atarpat el CBT")
+            time.sleep(2)
+            print("Era sarcasme. El CBT ha creat un vòrtex interdimensional")
+            time.sleep(2)
+            print("El CBT t'ha teletransportat. Tens sort de no haver mort")
+            CBT = False
+            zona = 1
+            
         
